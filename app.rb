4e49f84480a7c66ae8ebc5e0ab829cc7a1104b1b
rebeccanewborn <rjn58@cornell.edu>
2017-10-10 10:13:08 -0400

    done

--- app.rb
+++ app.rb
@@ -2,4 +2,26 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+
+    @puppy = Puppy.new(params["name"], params["breed"], params["age"])
+
+    erb :display_puppy
+  end
+
+  private
+
+  def make_puppy(hash)
+
+    newpuppy
+  end
+
 end
